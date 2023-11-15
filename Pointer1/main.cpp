57baeb8799e1339aac36cb19d51d4a64b5210d04
DimamonDESKTOP <dimamon02@gmail.com>
2023-11-15 20:03:30 +0200

    done2

--- Pointer1/main.cpp
+++ Pointer1/main.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <Windows.h>
 
+using namespace std;
+
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     int num1, num2;
 
-    std::cout << "Введіть перше число: ";
-    std::cin >> num1;
+    cout << "Введіть перше число: ";
+    cin >> num1;
 
-    std::cout << "Введіть друге число: ";
-    std::cin >> num2;
+    cout << "Введіть друге число: ";
+    cin >> num2;
 
     int* ptr1 = &num1;
     int* ptr2 = &num2;
 
     if (*ptr1 > *ptr2) {
-        std::cout << "Найбільше число: " << *ptr1 << std::endl;
+        cout << "Найбільше число: " << *ptr1 << endl;
     }
     else if (*ptr2 > *ptr1) {
-        std::cout << "Найбільше число: " << *ptr2 << std::endl;
+        cout << "Найбільше число: " << *ptr2 << endl;
     }
     else {
-        std::cout << "Числа рівні" << std::endl;
+        cout << "Числа рівні" << endl;
     }
 
     return 0;
