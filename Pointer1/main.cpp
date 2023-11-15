07d3871c2912b4a55fe6357c5221f34e92e17b60
DimamonDESKTOP <dimamon02@gmail.com>
2023-11-15 20:07:02 +0200

    done

---
+++ Pointer1/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int size = 5;
+    int sourceArray[size] = { 1, 2, 3, 4, 5 };
+    int destinationArray[size];
+
+    int* sourcePtr = sourceArray;
+    int* destPtr = destinationArray;
+
+    for (int i = 0; i < size; ++i) {
+        *destPtr = *sourcePtr;
+        ++sourcePtr;
+        ++destPtr;
+    }
+
+    cout << "Скопійований масив:" << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << destinationArray[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
