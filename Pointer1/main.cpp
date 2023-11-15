1a81463084b3977f4daf0e19bccc10da8beb18c3
DimamonDESKTOP <dimamon02@gmail.com>
2023-11-15 20:02:50 +0200

    done2

--- Pointer1/main.cpp
+++ Pointer1/main.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include <Windows.h>
 
+using namespace std;
+
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     int num;
 
-    std::cout << "¬вед≥ть число: ";
-    std::cin >> num;
+    cout << "¬вед≥ть число: ";
+    cin >> num;
 
     int* ptr = &num;
 
     if (*ptr > 0)
-        std::cout << "„исло додатнЇ." << std::endl;
+        cout << "„исло додатнЇ." << endl;
     else if (*ptr < 0)
-        std::cout << "„исло в≥д'Їмне." << std::endl;
+        cout << "„исло в≥д'Їмне." << endl;
     else
-        std::cout << "„исло нуль." << std::endl;
+        cout << "„исло нуль." << endl;
 
     return 0;
 }
