6f06858e0e5d1844bf2bd249991ef2e26d1e20d5
DimamonDESKTOP <dimamon02@gmail.com>
2023-11-15 20:02:10 +0200

    done2

--- Pointer1/main.cpp
+++ Pointer1/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <Windows.h>
+
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+
     int num1, num2;
 
-    std::cout << "Введіть перше число: ";
-    std::cin >> num1;
+    cout << "Введіть перше число: ";
+    cin >> num1;
 
-    std::cout << "Введіть друге число: ";
-    std::cin >> num2;
+    cout << "Введіть друге число: ";
+    cin >> num2;
 
     int* ptr1 = &num1;
     int* ptr2 = &num2;
@@ -18,9 +20,9 @@ int main() {
     *ptr1 = *ptr2;
     *ptr2 = temp;
 
-    std::cout << "Після обміну:" << std::endl;
-    std::cout << "Перше число: " << num1 << std::endl;
-    std::cout << "Друге число: " << num2 << std::endl;
+    cout << "Після обміну:" << endl;
+    cout << "Перше число: " << num1 << endl;
+    cout << "Друге число: " << num2 << endl;
 
     return 0;
 }
